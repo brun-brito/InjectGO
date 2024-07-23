e060b4366f9da813bf7cc5484bc0360cca449d62
brun-brito <brunocaudebrito@hotmail.com>
2024-07-23 00:40:45 -0300

    Speaker agora trabalha sempre em uma thread, ou seja, uma mesma conversa, com contexto

--- lib/subtelas/speaker.dart
+++ lib/subtelas/speaker.dart
@@ -3,13 +3,15 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:inject_go/subtelas/tutorial_speaker.dart';
 import 'package:bubble/bubble.dart';
-import 'package:openai_dart/openai_dart.dart' hide Image; // Esconde a classe Image do openai_dart
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+// import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+// import 'package:openai_dart/openai_dart.dart' hide Image;
 
 class ChatPage extends StatefulWidget {
   final String username;
@@ -24,33 +26,37 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> with SingleTickerProviderStateMixin {
   final TextEditingController _controller = TextEditingController();
   final List<Map<String, String>> _messages = [];
-  late OpenAI openAI;
   bool _isLoading = false;
   late AnimationController _animationController;
   late Animation<int> _dotsAnimation;
   final String _loadingText = "Speaker está digitando";
   late String assistantId;
-  late OpenAIClient client;
+  late String token;
+  late String orgId;
   String? tempImageUrl;   
   bool _isUploadingImage = false;
+  String? _threadId;
+  // late OpenAI openAI;
+  // late OpenAIClient client;
+
 
   @override
   void initState() {
     super.initState();
     dotenv.load().then((_) {
-      String token = dotenv.get('API_GPT');
-      String orgId = dotenv.get('ORG_ID');
+      token = dotenv.get('API_GPT');
+      orgId = dotenv.get('ORG_ID');
       assistantId = dotenv.get('ASSISTANT_ID');
-      client = OpenAIClient(
-        apiKey: token,
-        organization: orgId,
-      );
-      openAI = OpenAI.instance.build(
-        token: token,
-        orgId: orgId,
-        baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
-        enableLog: true,
-      );
+      // client = OpenAIClient(
+      //   apiKey: token,
+      //   organization: orgId,
+      // );
+      // openAI = OpenAI.instance.build(
+      //   token: token,
+      //   orgId: orgId,
+      //   baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
+      //   enableLog: true,
+      // );
     });
 
     _animationController = AnimationController(
@@ -67,7 +73,7 @@ class _ChatPageState extends State<ChatPage> with SingleTickerProviderStateMixin
     super.dispose();
   }
 
-Future<void> _sendMessage() async {
+/*  Future<void> _sendMessage2() async {
     String message = _controller.text;
     String? imageUrl = tempImageUrl;
     _controller.clear(); 
@@ -121,6 +127,139 @@ Future<void> _sendMessage() async {
 
       setState(() {
         _messages.add({'role': 'bot', 'content': responseData ?? 'Erro ao buscar resposta. Tente novamente ou volte mais tarde!', 'type': 'text'});
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _messages.add({
+          'role': 'bot',
+          'content': 'Erro ao se comunicar com o Speaker. Tente novamente mais tarde.',
+          'type': 'text'
+        });
+        _isLoading = false;
+      });
+    }
+  }
+*/
+  
+  Future<void> _sendMessage() async {
+    String message = _controller.text;
+    String? imageUrl = tempImageUrl;
+    _controller.clear();
+
+    setState(() {
+      if (message.isNotEmpty) {
+        _messages.add({'role': 'user', 'content': message, 'type': 'text'});
+      }
+      if (imageUrl != null) {
+        _messages.add({'role': 'user', 'content': imageUrl, 'type': 'image'});
+        tempImageUrl = null;
+      }
+      _isLoading = true;
+    });
+
+    final List<Map<String, dynamic>> content = [];
+    final Map<String, String> headers = {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+          'OpenAI-Beta': 'assistants=v2'
+        };
+
+    if (message.isNotEmpty) {
+      content.add({"type": "text", "text": message});
+    }
+    if (imageUrl != null) {
+      content.add({
+        "type": "image_url",
+        "image_url": {"url": imageUrl}
+      });
+    }
+
+    String apiUrl = 'https://api.openai.com/v1/threads';
+
+    try {
+      // Passo 1: Se não tiver criado, criar a thread
+      if (_threadId == null){
+        final createThreadResponse = await http.post(
+          Uri.parse(apiUrl),
+          headers: headers,
+          body: jsonEncode({}),
+        );
+
+        if (createThreadResponse.statusCode != 200) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Erro inesperado, tente novamente mais tarde.")),
+          );
+        }
+
+        final threadData = jsonDecode(createThreadResponse.body);
+        _threadId = threadData['id'];
+      }
+
+      // Passo 2: Criar a mensagem
+      final createMessageResponse = await http.post(
+        Uri.parse('$apiUrl/$_threadId/messages'),
+        headers: headers,
+        body: jsonEncode({
+          'role': 'user',
+          'content': content,
+        }),
+      );
+
+      if (createMessageResponse.statusCode != 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erro inesperado, tente novamente mais tarde.")),
+        );
+      }
+
+      // Passo 3: Criar a run
+      final createRunResponse = await http.post(
+        Uri.parse('$apiUrl/$_threadId/runs'),
+        headers: headers,
+        body: jsonEncode({
+          'assistant_id': assistantId,
+        }),
+      );
+      if (createRunResponse.statusCode != 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erro inesperado, tente novamente mais tarde.")),
+        );
+      }
+      // Aguardar a conclusão da run, ate ficar 'completed'
+      String runStatus = jsonDecode(createRunResponse.body)['status'];
+      String runId = jsonDecode(createRunResponse.body)['id'];
+      while (runStatus != 'completed') {
+        await Future.delayed(const Duration(seconds: 2));
+        final updatedRunResponse = await http.get(
+          Uri.parse('$apiUrl/$_threadId/runs/$runId'),
+          headers: {
+            'Authorization': 'Bearer $token',
+            'OpenAI-Beta': 'assistants=v2'
+          },
+        );
+        runStatus = jsonDecode(updatedRunResponse.body)['status'];
+      }
+      // Passo 4: Pegar a lista de mensagens
+      final getMessagesResponse = await http.get(
+        Uri.parse('$apiUrl/$_threadId/messages'),
+        headers: headers,
+      );
+
+      if (getMessagesResponse.statusCode != 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erro inesperado, tente novamente mais tarde.")),
+        );
+      }
+
+      final listaMensagens = jsonDecode(getMessagesResponse.body);
+      final responseData = utf8.decode(listaMensagens['data'][0]['content'][0]['text']['value'].runes.toList());
+
+      setState(() {
+        _messages.add({
+          'role': 'bot',
+          'content': responseData,// ?? 'Erro ao buscar resposta. Tente novamente ou volte mais tarde!',
+          'type': 'text'
+        });
         _isLoading = false;
       });
     } catch (error) {
@@ -234,7 +373,7 @@ Future<void> _sendMessage() async {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           if (message['type'] == 'text')
-                            Text(
+                            SelectableText(
                               message['content']!,
                               style: TextStyle(
                                 color: isUserMessage ? Colors.black : Colors.white,
